fde38994d986c152adfbb40dc1428ee9549f87a7
Demetrius de Castro do Amaral <128837011+DemetriusdeCastro@users.noreply.github.com>
2023-11-07 21:42:37 -0300

    Add files via upload

--- adicionar.php
+++ adicionar.php
@@ -1,3 +1,50 @@
+<?php
+
+session_start();
+
+include('conexao.php');
+include('funcoes.php');
+
+$nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+$cpf = isset($_POST['cpf']) ? $_POST['cpf'] : '';
+$telefone = isset($_POST['login']) ? $_POST['login'] : '';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+
+$selectcpf = "SELECT cpf FROM usuario WHERE cpf = '$cpf'";
+$querycpf = mysqli_query($conexao, $selectcpf);
+$dadocpf = mysqli_fetch_row($querycpf);
+
+$selectlogin = "SELECT login FROM login WHERE login = '$login'";
+$querylogin = mysqli_query($conexao, $selectlogin);
+$dadologin = mysqli_fetch_row($querylogin);
+
+if ($nome <> NULL) {
+	if (($dadocpf == NULL) && ($dadologin == NULL)) {
+		$insertusuario = "INSERT INTO usuario (nome, cpf, telefone)
+			VALUES
+			('$nome', '$cpf', '$telefone')";
+		$queryusuario = mysqli_query($conexao, $insertusuario);
+
+		$senhacriptografada = criptografar($senha);
+
+		$insertlogin = "INSERT INTO login (cpf, login, senha, nivel)
+		VALUES
+		('$cpf', '$login', '$senhacriptografada', 3)";
+		$querylogin = mysqli_query($conexao, $insertlogin);
+
+		echo '<script>alert("Usuário cadastrado com sucesso");
+			window.location="adicionar.php";
+			</script>';
+	} else {
+		echo '<script>alert("CPF e/ou Login já cadastrados no sistema");
+			window.location="adicionar.php";
+			</script>';
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +54,7 @@
 </head>
 <body>
 	<center>
+		<h1>Adicionar Usuário</h1>
 		<form id="form-add" action="#" method="POST">
 			Nome: <input type="text" name="nome"><br>
 			CPF: <input type="text" name="cpf"><br>
--- conexao.php
+++ conexao.php
@@ -1,14 +1,15 @@
 <?php
 
+/*
 $conexao = mysqli_connect('localhost', 'root', '', 'seguranca_2', '3306');
 if (!$conexao) {
     die('Não foi possível conectar');
 }
+*/
 
-/*
 $conexao = mysqli_connect('localhost', 'root', 'blink182', 'seguranca_2', '3310');
 if (!$conexao) {
     die('Não foi possível conectar');
-} */
+}
 
 ?>
